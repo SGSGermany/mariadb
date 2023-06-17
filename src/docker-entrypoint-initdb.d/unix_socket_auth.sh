8857ec632b2d87b1e589ff64fcf8d410e96640b7
Daniel Rudolf <github.com@daniel-rudolf.de>
2023-06-18 00:55:00 +0200

    Add support for MARIADB_ROOT_PASSWORD_HASH env variable

--- src/docker-entrypoint-initdb.d/unix_socket_auth.sh
+++ src/docker-entrypoint-initdb.d/unix_socket_auth.sh
@@ -10,9 +10,15 @@
 # License-Filename: LICENSE
 
 _unix_socket_auth() {
-    local MARIADB_ROOT_PASSWORD_ESCAPED="$(docker_sql_escape_string_literal "$MARIADB_ROOT_PASSWORD")"
+    local MARIADB_ROOT_PASSWORD_SQL
+    if [ -n "$MARIADB_ROOT_PASSWORD_HASH" ]; then
+        MARIADB_ROOT_PASSWORD_SQL="'$(docker_sql_escape_string_literal "$MARIADB_ROOT_PASSWORD_HASH")'"
+    else
+        MARIADB_ROOT_PASSWORD_SQL="PASSWORD('$(docker_sql_escape_string_literal "$MARIADB_ROOT_PASSWORD")')"
+    fi
+
     docker_process_sql \
-        <<<"ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket OR mysql_native_password USING PASSWORD('$MARIADB_ROOT_PASSWORD_ESCAPED')"
+        <<<"ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket OR mysql_native_password USING $MARIADB_ROOT_PASSWORD_SQL"
 }
 
 _unix_socket_auth
